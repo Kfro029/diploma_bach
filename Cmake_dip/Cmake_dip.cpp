f00e809f4d8a2009f95b2f32bb71a0ad420e728b
Кирилл Алаторцев <alatortsev.kv@phystech.su>
2024-09-25 20:08:30 +0300

    E считается функцией

--- Cmake_dip/Cmake_dip.cpp
+++ Cmake_dip/Cmake_dip.cpp
@@ -3,9 +3,24 @@
 
 #include <iostream>
 #include <vector>
+#include "Cmake_dip.h" 
 
-int main()
-{
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
+// Число частиц
+int N = 10;
+
+
+int main(){
+
+	std::vector<float> X_ions;
+	std::vector<float> X_el;
+	std::vector<float> V_ions (N, 0.0);
+	std::vector<float> V_el(N, 0.0);
+
+	for (int i = 0; i < N; i++) {
+		int L_int = L;
+		X_ions.push_back(rand() % (L_int+1));
+		X_el.push_back(rand() % (L_int + 1));
+	}
+
+	
 }
--- Cmake_dip/Cmake_dip.h
+++ Cmake_dip/Cmake_dip.h
@@ -2,7 +2,41 @@
 // или включаемые файлы для конкретного проекта.
 
 #pragma once
+#include <vector>
+#include <cmath>
 
-#include <iostream>
+// Число частиц
+int N = 10;
+
+//Основные константы
+double m_ion = 1.673 * pow(10, -27); 
+double m_el = 9.31 * pow(10, -31);
+double q = 1.6 * pow(10, -19);
+
+
+// шаг по времени и время
+double dt = 0.1;
+double T = 10.0;
+
+//Длина
+double L = 10.0;
+
+//Электрическое поле
+double E0 = 1;
+double E(double x) {
+	return (E0 * (x - L / 2));
+}
+
+void Move(std::vector<double> V_ions, std::vector<double> V_el, std::vector<double> X_ions, std::vector<double> X_el) {
+	for (double tim = 0.0; tim < T; tim += dt) {
+		for (std::size_t i = 0; i < X_ions.size(); i++) {
+			V_ions[i] += E(X_ions[i]) * q / m_ion * dt;
+			V_el[i] -= E(X_el[i]) * q / m_el * dt;
+			X_ions[i] += V_ions[i] * dt;
+			X_el[i] += V_el[i] * dt;
+			
+		}
+	}
+}
 
 // TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
